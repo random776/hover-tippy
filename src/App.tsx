c2852c45e6decd917b0a7ce8be3c47c274ac4a2b
random776 <125573400+random776@users.noreply.github.com>
2024-03-13 22:15:48 +0900

    add comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import Tippy from "@tippyjs/react";
+import Tippy from "@tippyjs/react"; // ホバーすると出てくるやつ
 import "tippy.js/dist/tippy.css";
 import { useEffect, useState } from "react";
-import Markdown from "react-markdown";
+import Markdown from "react-markdown"; // markdownからreactnodeへ変換（パース）するもの。
 import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
+import remarkMath from "remark-math"; // 数式の表示を可能とするものたち。
 import "katex/dist/katex.min.css";
 
 export default function App() {
   const [text, setText] = useState("");
 
   useEffect(() => {
-    fetch("markdown/test.md")
+    fetch("markdown/test.md") // markdownのデータを取得
       .then((response) => response.text()) // Extract text from response
       .then((data) => setText(data)) // Set text state
       .catch((error) => console.error("Error fetching data:", error));
@@ -19,9 +19,9 @@ export default function App() {
   return (
     <>
       <Tippy
-        content={
+        content={ // markdown -> reactnode
           <Markdown rehypePlugins={[rehypeKatex]} remarkPlugins={[remarkMath]}>
-            {text}
+            {text} 
           </Markdown>
         }
       >
