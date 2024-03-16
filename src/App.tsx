e2f4f4c305f99dc16b13feb6e6094c7f089b1ea5
random776 <125573400+random776@users.noreply.github.com>
2024-03-16 17:30:12 +0900

    made function to split markdown

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math"; // 数式の表示を可能とするものたち。
 import "katex/dist/katex.min.css";
 import TestMd from "/test.md?url";
+import { mdModify } from "./utils/mdModify";
+import Hoge from "/hoge.html?url";
+// import parse, { DOMNode, domToReact } from "html-react-parser";
+// import { JSX } from "react/jsx-runtime";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [html, setHtml] = useState("");
 
   useEffect(() => {
     fetch(TestMd) // markdownのデータを取得
@@ -17,18 +22,33 @@ export default function App() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  useEffect(() => {
+    fetch(Hoge) // htmlのデータを取得
+      .then((response) => response.text()) // Extract text from response
+      .then((data) => setHtml(data)) // Set text state
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  // const rep = html.replace(
+  // "ut.code();Learn",
+  // "<Tippy content={<Markdown rehypePlugins={[rehypeKatex]} remarkPlugins={[remarkMath]}>{mdModify(text, 'ut.code();Learn')}</Markdown>}<p>ut.code();Learn</p></Tippy>"
+  // );
+
   return (
     <>
       <Tippy
-        content={ // markdown -> reactnode
+        content={
           <Markdown rehypePlugins={[rehypeKatex]} remarkPlugins={[remarkMath]}>
-            {text} 
+            {mdModify(text, "ut.code();Learn")}
           </Markdown>
         }
       >
         <p>ut.code();Learn</p>
       </Tippy>
+
+      <p>
+        <iframe width="500" height="400" srcDoc={html}></iframe>
+      </p>
     </>
   );
 }
-
---
+++ src/utils/mdModify.tsx
@@ -0,0 +1,19 @@
+export function mdModify(text: string, title: any) {
+  const sections = text.split("##");
+  sections.shift();
+  const modifiedSections =
+    sections !== undefined
+      ? sections.map((item: string | null | undefined) => {
+          const titleAndContent = (item ?? "").split("\n");
+          const title = titleAndContent[0].trim();
+          const content = "## " + titleAndContent.slice(0).join("\n").trim();
+          return { title, content };
+        })
+      : [];
+
+  const learnSection = modifiedSections.find(
+    (section: { title: any }) => section.title === title
+  );
+  const learnContent = learnSection ? learnSection.content : "";
+  return learnContent;
+}
